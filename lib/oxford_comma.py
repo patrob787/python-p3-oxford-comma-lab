e109d752f1b1527f1870165aecbbf4237f777265
Patrick Roberts <patrob22@gmail.com>
2023-05-01 20:54:24 -0400

    lab complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        items.insert(1, 'and')
+        string = ' '.join(items)
+        return string
+    else:
+        new_items = ',  '.join(items).split('  ')
+        new_items.insert(len(items) - 1, 'and')
+        string = ' '.join(new_items)
+        return string
